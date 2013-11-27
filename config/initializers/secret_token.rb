44ce3539e3acdbc0918688797b2e0451506475bc
NCI <nci@nci-VirtualBox.(none)>
2013-11-27 01:14:03 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deport1::Application.config.secret_token = 'bd46bb027016d3e24baad970b0e9d8634f6557d51739fe394c3d14c2d903ac6518536b6cc767959337a3685e5f1c1225953b436f696cdac339d672c7a396ca50'
